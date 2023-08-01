8e9f1ef4c21bc39df456df50b0daa185e3fb87b9
maulanafikriash <maulanx835@gmail.com>
2023-08-01 10:49:54 +0700

    add function

--- hello.js
+++ hello.js
@@ -6,3 +6,14 @@ console.log(myBook);
 for (iterasiBook of myBook) {
   console.log(iterasiBook);
 }
+
+let number = 20
+function login(...data) {
+  if (data <= number) {
+    console.log(`data kurang dari ${number}`);
+  } else {
+    console.log(`data lebih dari ${number}`);
+  }
+}
+
+login(13);
